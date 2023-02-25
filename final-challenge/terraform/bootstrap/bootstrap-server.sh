e4598e1e55d7642242a006772b38050f95c35cc9
erickarroyo1 <ee.av@hotmail.com>
2023-02-25 13:57:27 -0500

    challenge-final/terraform/modules/kubernetes-fixes

--- final-challenge/terraform/bootstrap/bootstrap-server.sh
+++ final-challenge/terraform/bootstrap/bootstrap-server.sh
@@ -4,21 +4,39 @@
 sudo apt update -y
 sudo apt install curl net-tools inetutils-ping docker docker-compose git -y
 
+echo -e 'install docker and git\n'
 # docker and git
 
 sudo systemctl enable docker.service # inicia automáticamente (revisar***)
+echo '1'
 sudo systemctl start docker.service
+echo '2'
 sudo usermod -a -G docker ubuntu
-sudo id ubuntu
-sudo newgrp docker
+echo '3'
+
+echo -e 'fin docker and git\n'
+
+echo 'docker ps'
+
 docker ps  #ejecución sin root
+
+
+echo 'inicializar git y ejecutar docker'
 
 #clone git and start containers
 
 cd ~
+echo '1'
+rm -rf ~/bootcamp-challenge-final/
 sudo git clone https://github.com/erickarroyo1/bootcamp-challenge-final.git
+echo '2'
 cd bootcamp-challenge-final/final-challenge/Docker/
-docker-compose up - d
+echo -e '\ninicio docker-compose\n'
 
+docker-compose up -d
+sudo newgrp docker
+
+
+echo 'fin script bootstrap'
 #reboot instance 
-sudo reboot
+#sudo reboot
